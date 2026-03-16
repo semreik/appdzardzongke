e26e1fde05a64efde2839538f189e72516d2768f
semreik <57986762+semreik@users.noreply.github.com>
2026-03-16 21:30:29 +0300

    Delete lib directory

--- lib/db/sqlite.ts
+++
@@ -1,6 +0,0 @@
-import { Platform } from 'react-native';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const impl = Platform.OS === 'web' ? require('./sqlite.web') : require('./sqlite.native');
-export const openDb = impl.openDb;
-
-
